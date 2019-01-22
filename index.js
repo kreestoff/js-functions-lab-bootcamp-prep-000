daaada88770568e349810162aea9a5dd31b72f9f
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-01-22 22:53:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   `It\'s ${days} days until ${holiday}!`
 }
